634cedbf82fa38a985adb1145f0deb18aceef619
nrymarz <nrymarz@gmail.com>
2020-12-04 11:43:36 -0600

    commit tree 3d3a05eec5f7f8624593ccdbe7433a97a63d5504

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,17 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(name: params[:pet_name],owner_id: params[:owner_id])
+    if !params[:owner_name].empty?
+      owner =  Owner.create(name: params[:owner_name])
+      owner.pets << @pet
+      @pet.owner = owner
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +25,20 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'pets/edit'
+  end
 
+  patch '/pets/:id' do 
+    @pet = Pet.find_by_id(params[:id])
+    @pet.update(name: params[:pet_name], owner_id: params[:owner_id])
+    if !params[:owner][:name].empty?
+      owner =  Owner.create(params[:owner])
+      owner.pets << @pet
+      @pet.owner = owner
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
